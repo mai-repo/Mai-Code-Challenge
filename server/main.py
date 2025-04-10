f7bc7b4e1b317e529148f0ca8f0b002f7f4ad245
Mai <60695595+mai-repo@users.noreply.github.com>
2025-04-10 15:52:20 -0400

    Remove unused database connection logic

--- server/main.py
+++ server/main.py
@@ -1,33 +1,9 @@
 from flask import Flask, jsonify, request
 from GPT import generateCodeChallenge, evaluateProblem
-import psycopg2
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
+from questionsTable import questions
 
 app = Flask(__name__)
-
-def connectDatabase():
-    DATABASE_HOST = os.getenv("DATABASE_HOST")
-    DATABASE_PORT = os.getenv("DATABASE_PORT")
-    DATABASE_NAME = os.getenv("DATABASE_NAME")
-    DATABASE_USER = os.getenv("DATABASE_USER")
-    DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD")
-
-    try:
-        connection = psycopg2.connect(
-                host=DATABASE_HOST,
-                port=DATABASE_PORT,
-                database=DATABASE_NAME,
-                user=DATABASE_USER,
-                password=DATABASE_PASSWORD
-        )
-        return connection
-    except psycopg2.Error as e:
-        print(f"Error connecting to PostgreSQL: {e}")
-
-
+app.register_blueprint(questions)
 
 @app.get("/generateChallenge")
 def getChallenge():
@@ -55,10 +31,8 @@ def evaluateAnswer():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-
-
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=80)
 
 
 
