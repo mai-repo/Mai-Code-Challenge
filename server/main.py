bf4be8be5ab5b927fffb9a25f5dd6bbcbbddd116
Mai <60695595+mai-repo@users.noreply.github.com>
2025-04-11 13:30:31 -0400

    Add reCAPTCHA verification endpoint and enhance rate limiting for existing routes

--- server/main.py
+++ server/main.py
@@ -4,14 +4,35 @@ from questions import questions
 from users import users
 from rejected import rejected
 from completed import completed
+from favorite import favorite
+import requests
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+from dotenv import load_dotenv
+import os
+import logging
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
 app.register_blueprint(questions)
 app.register_blueprint(users)
 app.register_blueprint(rejected)
 app.register_blueprint(completed)
+app.register_blueprint(favorite)
+
+limiter = Limiter(
+    get_remote_address,
+    app=app,
+    default_limits=["100 per hour"],
+)
+
+load_dotenv()
+BACKEND_KEY = os.getenv("BACKEND_KEY")
 
 @app.get("/generateChallenge")
+@limiter.limit("10+ per minute")
 def getChallenge():
     try:
         codeChallenge = generateCodeChallenge()
@@ -23,6 +44,7 @@ def getChallenge():
         return jsonify({"error": str(e)}), 500
 
 @app.post("/evaluateAnswer")
+@limiter.limit("10 per minute")
 def evaluateAnswer():
     try:
         data = request.get_json()
@@ -34,6 +56,29 @@ def evaluateAnswer():
 
         evaluation = evaluateProblem(challenge, answer)
         return jsonify(evaluation), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+@app.route('/verifyUser', methods=['POST'])
+def verify_user():
+    try:
+        data = request.get_json()
+        token = data.get('token')
+        secret = BACKEND_KEY
+
+        recaptcha_response = requests.post(
+            'https://www.google.com/recaptcha/api/siteverify',
+            data={
+                'secret': secret,
+                'response': token
+            }
+        )
+        result = recaptcha_response.json()
+
+        if result.get('success'):
+            return jsonify({"message":'reCAPTCHA verified successfully!'}), 200
+        else:
+            return jsonify({"message":'Failed to verify reCAPTCHA.'}), 400
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
