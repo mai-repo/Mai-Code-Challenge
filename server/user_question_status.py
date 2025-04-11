d32bf9f0f1362e4733315f920cc7aef200090670
Mai <60695595+mai-repo@users.noreply.github.com>
2025-04-11 15:09:29 -0400

    Add functions to add and update user question status

--- server/user_question_status.py
+++ server/user_question_status.py
@@ -28,3 +28,52 @@ def getStatus():
             cursor.close()
             connection.close()
 
+
+def addStatus(user_id, question_id, status):
+
+    if status not in ['COMPLETED', 'REJECTED']:
+        return jsonify({'error': 'Invalid status value'}), 400
+
+    if not all([user_id, question_id, status]):
+        return jsonify({"error": "Missing field informaiton."}), 400
+
+    try:
+        connection = connectDatabase()
+        cursor = connection.cursor()
+
+        cursor.execute('''
+                        INSERT INTO USER_QUESTION_STATUS (USER_ID, QUESTION_ID, STATUS)
+                        VALUES(%s, %s, %s)
+                       ''', (user_id, question_id, status))
+        connection.commit()
+        return jsonify({"message": "Status added successfully"}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    finally:
+        cursor.close()
+        connection.close()
+
+def updateStatus(user_id, id, status):
+
+    if status not in ['COMPLETED', 'REJECTED']:
+        return jsonify({'error': 'Invalid status value'}), 400
+
+    if not all([user_id, id, status]):
+        return jsonify({"error": "Missing field informaiton."}), 400
+
+    try:
+        connection = connectDatabase()
+        cursor = connection.cursor()
+
+        cursor.execute('''
+                        UPDATE USER_QUESTION_STATUS
+                        SET STATUS = %s
+                        WHERE USER_ID = %s AND QUESTION_ID = %s
+                       ''', (status, user_id, id))
+        connection.commit()
+        return jsonify({"message": "Status added successfully"}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    finally:
+        cursor.close()
+        connection.close()
