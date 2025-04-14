e3165553ec8bb5c67641d6e41673332a55d404ca
Mai <60695595+mai-repo@users.noreply.github.com>
2025-04-14 18:57:06 -0400

    Implement searchFavorite endpoint to retrieve favorite problems based on user input; enhance error handling for search results.

--- server/favorite.py
+++ server/favorite.py
@@ -116,3 +116,38 @@ def deleteFavorite():
         cursor.close()
         connection.close()
 
+@favorite.get('/searchFavorite')
+def searchFavorite():
+    data = request.get_json()
+    user_id = data.get("user_id")
+    search_term = data.get("search_term").lower()
+
+    try:
+
+        connection = connectDatabase()
+        cursor = connection.cursor()
+
+        cursor.execute('''
+                        SELECT FAVORITE_PROBLEMS
+                        FROM FAVORITES
+                        WHERE USER_ID = %s
+                        ''', (user_id,))
+        favorite_problems = cursor.fetchall()
+
+        problem_ids = [[row[0]] for row in favorite_problems]
+
+        cursor.execute('''
+            SELECT *
+            FROM QUESTIONS
+            WHERE ID = ANY(%s)
+            ''', (problem_ids,))
+
+        questions = cursor.fetchall()
+
+        for question in questions:
+            if search_term == question[2]:
+                return jsonify({"id": question[0], "name": question[2]}), 200
+            else:
+                return jsonify({"error": "No search found."})
+    except Exception as e:
+        jsonify({"error": str(e)})
