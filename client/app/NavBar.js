c395adb050cbc6f5dfa306eb86aee3bfb7a4d328
Mai <60695595+mai-repo@users.noreply.github.com>
2025-04-16 17:41:35 -0400

    Add NavBar component with Flowbite integration

---
+++ client/app/NavBar.js
@@ -0,0 +1,12 @@
+import { Navbar } from "flowbite-react"
+export default function NavBar(){
+
+    return (
+        <main>
+            <Navbar fluid rounded className="p-5 border-b-2 border-black">
+                <img src="/mai_code_challenge_logo.png" className="w-1/3"/>
+            </Navbar>
+
+        </main>
+    )
+}
