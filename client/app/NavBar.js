bb1ea4046528a5fa33c031f0ba850d610d8bd101
Mai <60695595+mai-repo@users.noreply.github.com>
2025-04-17 02:33:53 -0400

    Wrap logo image in NavbarBrand component for improved navigation

--- client/app/NavBar.js
+++ client/app/NavBar.js
@@ -1,10 +1,14 @@
-import { Navbar } from "flowbite-react"
+import { Navbar, NavbarBrand} from "flowbite-react"
+import Link from "next/link"
 export default function NavBar(){
 
     return (
         <main>
             <Navbar fluid rounded className="p-5 border-b-2 border-black">
-                <img src="/mai_code_challenge_logo.png" className="w-1/3"/>
+                <NavbarBrand as={Link} href="/authentication/login">
+                <img src="/mai_code_challenge_logo.png" className="w-1/3" alt="mai_code_challenge_logo"/>
+                </NavbarBrand>
+
             </Navbar>
         </main>
     )
