8e252eff054cf01fbb4a0c1d8f8dc7270eb390af
Mai <60695595+mai-repo@users.noreply.github.com>
2025-04-18 13:06:28 -0400

    Add Scrape functionality to CodeButton component

--- client/app/challenge/CodeButton.js
+++ client/app/challenge/CodeButton.js
@@ -1,12 +1,12 @@
 "use client"
 import { Button } from "flowbite-react"
 
-export default function CodeButton({GPTGenerate, Clear}) {
+export default function CodeButton({ Scrape, GPTGenerate, Clear}) {
 
     return (
 
         <section className="flex flex-row justify-between">
-            <Button className="text-lg bg-red-950">Generate</Button>
+            <Button className="text-lg bg-red-950" onClick={Scrape}>Generate</Button>
             <Button className="text-lg bg-teal-700" onClick={GPTGenerate}>GPT Generate</Button>
             <Button className="text-lg bg-amber-500 text-black" onClick={Clear}> Clear </Button>
         </section>
