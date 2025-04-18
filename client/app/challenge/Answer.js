5c65bd05850ec8c850b3534d703eb9e5cc642dbb
Mai <60695595+mai-repo@users.noreply.github.com>
2025-04-18 13:08:26 -0400

    Add Answer component to evaluate and save challenge responses

---
+++ client/app/challenge/Answer.js
@@ -0,0 +1,61 @@
+"use client"
+import { useState } from 'react'
+import { useAppContext } from 'components/context'
+import { Button } from 'flowbite-react'
+
+export default function Answer() {
+    const { id, data, value, setValue } = useAppContext()
+    const [result, setResult] = useState([])
+
+    const getAnswer = async() => {
+
+        console.log(data.Challenge, value)
+        try {
+            const res = await fetch ("https://backendcodechallenge.vercel.app/evaluateAnswer", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    challenge: data.Challenge,
+                    answer: value
+                })
+            })
+            const resultData = await res.json()
+            setResult(resultData.isCorrect)
+            const evaluation = resultData.breakdown.join('\n');
+            setValue(evaluation)
+            alert("Evaluation successful.")
+        } catch (error) {
+            console.error(error.message)
+        }
+    }
+
+    const addQuestion = async () => {
+        try {
+            const res = await fetch ("https://backendcodechallenge.vercel.app/addProblem", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    user_id: id,
+                    name: data.Name,
+                    problem: data.Challenge,
+                    is_correct: result
+                })
+            })
+            const response = await res.json()
+            console.log(response)
+        } catch (error) {
+            console.error(error.message)
+        }
+    }
+
+    return (
+        <>
+            <Button  className="text-lg bg-purple-950" onClick={getAnswer}> Evaluate </Button>
+            <Button className="text-lg bg-pink-700" onClick={addQuestion}> Save Question </Button>
+        </>
+    )
+}
