cb9422804bc5cb990e69834dfa1bfb4172fbfb8f
Mai <60695595+mai-repo@users.noreply.github.com>
2025-04-18 13:07:14 -0400

    Refactor Terminal component to use context for state management and add scraping functionality

--- client/app/challenge/Terminal.js
+++ client/app/challenge/Terminal.js
@@ -1,12 +1,14 @@
 "use client"
 import { useState } from "react";
 import CodeButton from "./CodeButton"
-
+import { useAppContext } from "components/context";
 
 export default function Terminal(){
-        const [data, setData] = useState({});
+        const { id, data, setData } = useAppContext()
+        const [scrape, setScrape] = useState([])
 
         async function GPTGenerate() {
+
             try {
                 const response = await fetch(
                     "https://backendcodechallenge.vercel.app/generateChallenge"
@@ -19,25 +21,53 @@ export default function Terminal(){
             }
         }
 
+        async function Scrape() {
+
+            try {
+                const response = await fetch (
+                    "https://backendcodechallenge.vercel.app/scrape"
+                )
+                const result = await response.json();
+                setScrape(result);
+                console.log(result);
+
+            } catch (error) {
+                console.error ("Error fetch data:", error)
+            }
+        }
+
+        const cleanText = (text) => {
+            return text.replace(/\$(.*?)\$/g, (match, p1) => p1);
+        };
+
+
     return (
         <main className="flex flex-col">
             <section className="flex flex-col flex-1">
-                <div className="bg-black p-20 h-155 mb-4 text-white overflow-auto rounded-lg">
-                {data ? (
+            <div className="bg-black p-20 h-155 mb-4 text-white overflow-auto rounded-lg">
+                    {data ? (
                         <div className="mb-6">
-                        <p><strong>Name:</strong> {data.Name}</p> <br/>
-                        <p><strong>Problem:</strong> {data.Challenge}</p> <br/>
-                        <p><strong>Constraints:</strong> {data.Constraints}</p>  <br/>
-                        <p><strong>Input:</strong> {data.Input}</p>  <br/>
-                        <p><strong>Output:</strong> {data.Output}</p>  <br/>
+                            <p>{data.Name}</p><br />
+                            <p>{data.Challenge}</p><br />
+                            <p>{data.Constraints}</p><br />
+                            <p>{data.Input}</p><br />
+                            <p>{data.Output}</p><br />
                         </div>
                     ) : (
                         <p className="text-white italic">Click "Generate Again to Load a Problem"</p>
+                    )}
+                    {scrape.problem_content && (
+                        scrape.problem_content.map((problem, index) => (
+                            <div key={index}>
+                                <p>{cleanText(problem)}</p> <br/>
+                            </div>
+                        ))
                     )}
                 </div>
                 <CodeButton
                 GPTGenerate={GPTGenerate}
                 Clear = {() => setData("")}
+                Scrape={Scrape}
                 />
             </section>
         </main>
