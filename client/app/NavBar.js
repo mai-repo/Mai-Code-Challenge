1cd8a698404eb5626e069c17fb186d7e9788439c
Mai <60695595+mai-repo@users.noreply.github.com>
2025-04-19 21:49:54 -0400

    Update NavBar link to point to the home page

--- client/app/NavBar.js
+++ client/app/NavBar.js
@@ -5,7 +5,7 @@ export default function NavBar(){
     return (
         <main>
             <Navbar fluid rounded className="p-5 border-b-2 border-black">
-                <NavbarBrand as={Link} href="/authentication/login">
+                <NavbarBrand as={Link} href="/">
                 <img src="/mai_code_challenge_logo.png" className="w-1/3" alt="mai_code_challenge_logo"/>
                 </NavbarBrand>
 
