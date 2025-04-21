c2227bc3e085bd983f1dc67f3a8a4af259d1ecfb
Mai <60695595+mai-repo@users.noreply.github.com>
2025-04-21 18:08:55 -0400

    Add onClick handler to NavbarBrand for sign-out functionality

--- client/app/NavBar.js
+++ client/app/NavBar.js
@@ -26,7 +26,7 @@ export default function NavBar(){
     return (
         <main>
             <Navbar fluid rounded className="p-5 border-b-2 border-black">
-                <NavbarBrand href="/">
+                <NavbarBrand href="/" onClick={(e) => { setId(null); setUid(null); setValue(null); setData(null)}}>
                     <img src="/mai_code_challenge_logo.png" className="pl-8 w-1/3" alt="mai_code_challenge_logo"/>
                 </NavbarBrand>
             { id && (
