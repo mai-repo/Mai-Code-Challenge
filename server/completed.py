5eb0896e595e85ff603a8fda7ad253029877074b
Mai <60695595+mai-repo@users.noreply.github.com>
2025-04-21 15:36:48 -0400

    Refactor getCompleted to return paginated results

--- server/completed.py
+++ server/completed.py
@@ -1,5 +1,6 @@
 from db_connection import connectDatabase
 from flask import Blueprint, jsonify, request
+from pagination import paignation
 import logging
 
 logger = logging.getLogger(__name__)
@@ -39,7 +40,7 @@ def getCompleted():
 
         questions = cursor.fetchall()
 
-        return jsonify(questions)
+        return paignation(questions), 200
 
     except Exception as e:
         return jsonify({"error": str(e)}), 500
