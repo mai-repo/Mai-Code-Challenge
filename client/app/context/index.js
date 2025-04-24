a1104667481a90b5e8be3d81cc0f15cd03038138
Mai <60695595+mai-repo@users.noreply.github.com>
2025-04-23 22:06:21 -0400

    Add error and loading state management to AppContext

--- client/app/context/index.js
+++ client/app/context/index.js
@@ -8,7 +8,9 @@ export function AppWrapper({ children }) {
     const [uid, setUid] = useState(null);
     const [data, setData] = useState([])
     const [value, setValue] = useState('');
-    const [challenge, setChallenge] = useState('')
+    const [challenge, setChallenge] = useState('');
+    const [error, setError] = useState('');
+    const [loading, setLoading] =useState('');
 
 
     useEffect(() => {
@@ -17,11 +19,15 @@ export function AppWrapper({ children }) {
         const storedData = localStorage.getItem('data');
         const storedValue = localStorage.getItem('value');
         const storedChallenge = localStorage.getItem('challenge')
+        const storedError = localStorage.getItem('error');
+        const storedLoading = localStorage.getItem('loading');
         if (storedId) setId(Number(storedId));
         if (storedUid) setUid(storedUid);
         if (storedValue) setValue(storedValue);
         if (storedData) setData(storedData);
         if (storedChallenge) setChallenge(storedChallenge);
+        if (storedError) setError(storedError);
+        if (storedLoading) setLoading(storedLoading);
 
     }, []);
 
@@ -31,10 +37,12 @@ export function AppWrapper({ children }) {
         localStorage.setItem('data', data);
         localStorage.setItem('value', value);
         localStorage.setItem('challenge', challenge);
-    }, [id, uid, data, value]);
+        localStorage.setItem('error', error);
+        localStorage.setItem('loading', loading);
+    }, [id, uid, data, value, challenge, error, loading]);
 
     return (
-        <AppContext.Provider value={{ id, setId, uid, setUid, data, setData, value, setValue, challenge, setChallenge }}>
+        <AppContext.Provider value={{ id, setId, uid, setUid, data, setData, value, setValue, challenge, setChallenge, error, setError, loading, setLoading }}>
             {children}
         </AppContext.Provider>
     );
