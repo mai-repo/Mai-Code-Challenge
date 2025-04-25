c9bdba683f512d2284cc9a1df26d4b2d795dae5a
Mai <60695595+mai-repo@users.noreply.github.com>
2025-04-25 02:16:25 -0400

    Adjust UserSettings layout for improved readability and consistency

--- client/app/userSettings/page.js
+++ client/app/userSettings/page.js
@@ -119,31 +119,31 @@ export default function UserSettings(){
     }
 
     return (
-        <section className="flex flex-col justify-center items-center max-w-3/4  mx-20 p-12 bg-white border-2 border-black ">
+        <section className="flex flex-col justify-center items-center max-w-full  mx-10 p-12 bg-white border-2 border-black ">
             <div className="flex flex-col justify-center items-center w-full">
                 <section className="w-full">
-                    <div className="text-2xl mb-2">Change Name and Password</div>
-                    <Label className="text-lg">Name</Label>
+                    <div className="text-xl mb-2">Change Name and Password</div>
+                    <Label className="text-md">Name</Label>
                     <div className="flex justify-between gap-5">
                         <TextInput id="name" type="name" placeholder="Bad Bunny" value={username} onChange={(e) => setUsername(e.target.value)} className="mb-3 w-3/4"required icon={HiPencil}/>
                         <Button onClick={() => updateUsername(id, username)} className="w-1/6 text-lg bg-teal-500">Reset</Button>
                     </div>
                 </section>
                 <section className="w-full">
-                    <Label className="text-lg">Generate a new Password Link: Enter your email</Label>
-                    <div className="flex justify-between mb-12 gap-5">
+                    <Label className="text-md">Generate a new Password Link: Enter your email</Label>
+                    <div className="flex justify-between mb-8 gap-5">
                         <TextInput id="input" type="input" placeholder="Enter your email" value={email} onChange={(e) => setEmail(e.target.value)} className="w-3/4" icon={HiMail} required/>
                         <Button onClick={() => ResetPassword(email)} className="w-1/6 text-lg  bg-teal-500">Reset</Button>
                     </div>
-                    <section className="w-full mb-12">
-                        <div className="text-2xl mb-2"> Change Email </div>
-                        <Label className="text-lg"> New Email</Label>
+                    <section className="w-full mb-8">
+                        <div className="text-xl mb-2"> Change Email </div>
+                        <Label className="text-md"> New Email</Label>
                         <TextInput id="email" type="email" placeholder="name@gmail.com" value={newEmail} onChange={(e)=> setNewEmail(e.target.value)} className="mb-3 w-3/4" icon={HiMail}required/>
-                        <Label className="text-lg">Password</Label>
+                        <Label className="text-md">Password</Label>
                         <TextInput id="password" type="password" placeholder="Enter Password" value={currentPassword} onChange={(e)=> setCurrentPassword(e.target.value)} className="mb-3 w-3/4" icon={HiOutlineKey}required/>
                         <Button onClick={() => changeEmail(newEmail, currentPassword)} className="w-1/4 text-lg bg-green-600">Reset</Button>
                     </section>
-                    <div className="text-2xl"> Delete Account  </div>
+                    <div className="text-xl"> Delete Account  </div>
                     <Button onClick={() => DeleteUser(id, uid)} className="w-1/4 bg-red-800 text-xl">Delete</Button>
                 </section>
             </div>
