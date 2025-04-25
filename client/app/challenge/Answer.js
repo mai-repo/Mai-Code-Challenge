08a12d0bf9e303c864b787dfe7aae20189fae337
Mai <60695595+mai-repo@users.noreply.github.com>
2025-04-25 11:56:59 -0400

    Remove console log and improve error handling in Answer component

--- client/app/challenge/Answer.js
+++ client/app/challenge/Answer.js
@@ -8,8 +8,6 @@ export default function Answer() {
     const [result, setResult] = useState([])
 
     const getAnswer = async() => {
-
-        console.log(data.Challenge, value)
         try {
             const response = await fetch ("https://backendcodechallenge.vercel.app/evaluateAnswer", {
                 method: "POST",
@@ -29,9 +27,8 @@ export default function Answer() {
             const evaluation = resultData.breakdown.join('\n');
             setValue(evaluation)
             alert("Evaluation successful.")
-            navigator.clipboard.writeText(value)
         } catch (error) {
-            alert(error+ "try again!")
+            alert(error+ " Try again!")
         }
     }
 
@@ -52,10 +49,6 @@ export default function Answer() {
             const response = await res.json()
             setData(response)
             alert(response.message)
-
-            if (!response.ok) {
-                throw new Error(data.error || 'An error occurred');
-            }
         } catch (error) {
             alert(error)
         }
