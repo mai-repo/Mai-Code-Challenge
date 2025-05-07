d2b809180ca76bc4afd90a5fc5e2955c8352e2ac
Mai <60695595+mai-repo@users.noreply.github.com>
2025-05-06 20:15:01 -0400

    Add test for forgot password page rendering and functionality

---
+++ client/tests/forgot.spec.js
@@ -0,0 +1,9 @@
+import { test, expect } from '@playwright/test';
+
+test('renders the page and the forgot', async ({ page }) => {
+    await page.goto("https://mai-code-challenge.vercel.app/authentication/forgot");
+    await page.locator('#email').fill('test@gmail.com');
+    await expect(page).toHaveTitle(/Mai Code Challenge/)
+    await page.getByRole('button', { name: 'Submit'}).click()
+    await expect(page.locator('label')).toContainText('Click here the button to reset your password');
+})
