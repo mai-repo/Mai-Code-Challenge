0933fc15053f7d0b773be753ef3bfcc1553519cf
Mai <60695595+mai-repo@users.noreply.github.com>
2025-05-07 18:14:07 -0400

    Import formatDate utility and update date display in favoritePage component

--- client/app/userSettings/favorite/page.js
+++ client/app/userSettings/favorite/page.js
@@ -3,6 +3,7 @@ import { useAppContext } from "components/context"
 import { useState, useEffect } from "react"
 import { useRouter } from "next/navigation";
 import { Button, TextInput, Pagination, Spinner} from "flowbite-react";
+import { formatDate } from "utils/validation";
 
 export default function favoritePage(){
     const {id, data, setData, setChallenge, setLoading} = useAppContext()
@@ -99,7 +100,7 @@ export default function favoritePage(){
                         <a href="#" onClick={(e) => { e.preventDefault(); getChallenge(item);}} className="text-xl text-blue-600 hover:underline">
                             <h2>{item[2]}</h2>
                         </a>
-                        <p className="flex justify-center items-center"> {new Date(item[4]).toLocaleString()}</p>
+                        <p className="flex justify-center items-center"> {formatDate(item[4])}</p>
                         {editingId === item[0] ? (
                             <div className="flex items-end gap-2 justify-end">
                                 <TextInput
